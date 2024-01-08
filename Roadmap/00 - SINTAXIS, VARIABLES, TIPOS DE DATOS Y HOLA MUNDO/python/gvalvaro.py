33ac9b7c8080ed40c982dc3442620a7348125268
Alvaro Garcia <alvaro.garciavelasco96@gmail.com>
2024-01-08 13:29:03 +0100

    Ejercicio 00 GVALVARO96 en Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/gvalvaro.py
@@ -0,0 +1,22 @@
+# https://www.python.org/
+
+# Comentario en una sola linea
+
+'''
+    Esto es 
+    un comentario 
+    en varias
+    lineas
+'''
+
+variable = "variable"
+CONSTANTE = "constante"
+
+numero = 1
+my_str = "Hola"
+myfloat = 1.2
+my_bool = False
+my_list = []
+my_dict = {}
+
+print("Hola Python !")
